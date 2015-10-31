7c49df02c2be3f1be5203e9790db1703d1935278
saitjr <tangjr.work@gmail.com>
2015-10-31 21:26:30 +0800

    json支持大小写

--- Application/Api/Conf/config.php
+++ Application/Api/Conf/config.php
@@ -10,8 +10,8 @@ return array(
     'DB_PWD'                =>  '12345',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'wp_',    // 数据库表前缀
-    'DB_PARAMS'          	=>  array(), // 数据库连接参数    
-    'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_PARAMS'             =>  array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL), // 数据库连接参数    
+    'DB_DEBUG'  	    =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
     'DB_DEPLOY_TYPE'        =>  0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
